40ed0549d903ca6fcffd2ff04dc118258ea46a54
Anabel Arbeeny <anabela0701@hstat.org>
2018-10-11 01:06:25 +0000

    final

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,23 +1,28 @@
-def division
+def division(num1,num2)
+    return num1 / num2
     
 end
 
-def argue
+def argue(saying)
+    return saying
   
 end
 
-def greeting
+def greeting(arg1 , arg2 )
+    
 
 end
 
 def return_a_value
+    return "Nice"
+end
+
+def pizza_party(topping = "cheese")
+   return topping
     
 end
 
-def pizza_party
-    
-end
-
-def whisper
+def whisper(arg)
+    return arg.downcase
     
 end
